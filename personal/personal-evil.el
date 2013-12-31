708c4c434cfc16bb935af9a2b670503fc2fc2338
Shaun Taheri <shaun@lshift.net>
2013-12-31 13:01:16 +0000

    add evil mode keybindings

--- personal/personal-evil.el
+++ personal/personal-evil.el
@@ -8,6 +8,8 @@
 (setq evil-want-C-u-scroll t)
 
 (require 'evil)
+(evil-mode 1)
+(setq evil-shift-width 2)
 
 ;; use on text-mode and prog-mode
 (add-hook 'text-mode-hook 'turn-on-evil-mode)
@@ -15,10 +17,23 @@
 (add-hook 'comint-mode-hook 'turn-on-evil-mode)
 (add-hook 'Info-mode-hook 'turn-off-evil-mode)
 
-(require 'evil)
+;; remap keys
+(defun evil-move-key (keymap-from keymap-to key)
+  "Moves key binding from one keymap to another, deleting from the old location."
+  (define-key keymap-to key (lookup-key keymap-from key))
+  (define-key keymap-from key nil))
 
-(evil-mode 1)
-(setq evil-shift-width 2)
+(evil-move-key evil-motion-state-map evil-normal-state-map (kbd "RET"))
+(evil-move-key evil-motion-state-map evil-normal-state-map " ")
+
+(define-key minibuffer-local-map "\C-p" nil)
+(define-key minibuffer-local-map "\C-n" nil)
+
+;; easier window resizing
+(define-key evil-normal-state-map "+" 'evil-window-increase-width)
+(define-key evil-normal-state-map "-" 'evil-window-decrease-width)
+(define-key evil-normal-state-map (kbd "C-+") 'evil-window-increase-height)
+(define-key evil-normal-state-map (kbd "C--") 'evil-window-decrease-height)
 
 
 (provide 'personal-evil)
