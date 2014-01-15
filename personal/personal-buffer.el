b34f3614fc029ad48e9981d3db33c7aaad124262
Shaun Taheri <shaun@lshift.net>
2014-01-15 12:31:38 +0000

    highlight symbol mode keybindings

--- personal/personal-buffer.el
+++ personal/personal-buffer.el
@@ -1,5 +1,11 @@
+;; auto revert buffers
 (global-auto-revert-mode 1)
 (setq auto-revert-check-vc-info t)
 
+;; highlight symbol mode
 (require 'highlight-symbol)
 (highlight-symbol-nav-mode)
+(global-set-key [(control f3)] 'highlight-symbol-at-point)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
