4e38be6d9bdb87b70e563f79e53db3d3f9d13439
Shaun Taheri <shaun@lshift.net>
2014-01-17 17:54:04 +0000

    Set speedbar keybinding

--- personal/personal-speedbar.el
+++ personal/personal-speedbar.el
@@ -1,6 +1,7 @@
 (require 'sr-speedbar)
 
 (setq sr-speedbar-right-side nil)
+(setq sr-speedbar-refesh-turn-off t)
 (setq speedbar-show-unknown-files t)
 
-;(global-set-key (kbd "C-x SPC") 'sr-speedbar-toggle)
+(global-set-key (kbd "<f4>") 'sr-speedbar-toggle)
