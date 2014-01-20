d8cc7faa0b4da38b55f65b0f760801f95a4cbcae
Shaun Taheri <shaun@lshift.net>
2014-01-20 14:23:25 +0000

    update git grep keybindings

--- personal/personal-speedbar.el
+++ personal/personal-speedbar.el
@@ -1,7 +1,7 @@
 (require 'sr-speedbar)
 
 (setq sr-speedbar-right-side nil)
-(setq sr-speedbar-refesh-turn-off t)
+;(setq sr-speedbar-refesh-turn-off t)
 (setq speedbar-show-unknown-files t)
 
 (global-set-key (kbd "<f4>") 'sr-speedbar-toggle)
