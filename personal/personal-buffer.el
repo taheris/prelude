f3dafd2254064065b94d28900ee4135ec606030d
Shaun Taheri <shaun@taheris.co.uk>
2014-03-26 12:01:45 +0000

    Add super key window keybindings

--- personal/personal-buffer.el
+++ personal/personal-buffer.el
@@ -9,3 +9,9 @@
 (global-set-key (kbd "<f3>") 'highlight-symbol-next)
 (global-set-key (kbd "<S-f3>") 'highlight-symbol-prev)
 (global-set-key (kbd "<M-f3>") 'highlight-symbol-query-replace)
+
+;; fast window switching
+(global-set-key (kbd "s-h") 'evil-window-left)
+(global-set-key (kbd "s-j") 'evil-window-down)
+(global-set-key (kbd "s-k") 'evil-window-up)
+(global-set-key (kbd "s-l") 'evil-window-right)
