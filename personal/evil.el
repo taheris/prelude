c1fd06927503b09526b63be7b908e7969d69f672
Shaun Taheri <git@taheris.co.uk>
2014-11-26 17:35:02 +0100

    add helm-find-files to evil-leader key f

--- personal/evil.el
+++ personal/evil.el
@@ -39,6 +39,7 @@
 (evil-leader/set-key "w" 'save-buffer)
 (evil-leader/set-key "q" 'kill-this-buffer)
 (evil-leader/set-key "d" 'dired-jump)
+(evil-leader/set-key "f" 'helm-find-files)
 (evil-leader/set-key "v" 'split-window-right)
 (evil-leader/set-key "e" 'pp-eval-last-sexp)
 (evil-leader/set-key "," 'other-window)
